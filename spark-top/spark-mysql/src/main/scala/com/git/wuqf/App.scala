4161549392adc868a4e88d31aba8f10a22e8a242
deadking01 <wuqingfeng234@163.com>
2016-11-03 18:41:31 +0800

    能够运行sql了

--- spark-top/spark-mysql/src/main/scala/com/git/wuqf/App.scala
+++ spark-top/spark-mysql/src/main/scala/com/git/wuqf/App.scala
@@ -6,12 +6,19 @@ import org.apache.spark.sql.{SQLContext, SparkSession}
   * Hello world!
   *
   */
-object App {
+object mysql {
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("spark-mysql").master("spark://hadoop01:7077").getOrCreate();
-    val jdbcDF = spark.sqlContext.load("jdbc", Map("url" -> "jdbc:mysql://192.168.0.118:3306/resource?user=root&password=sdzn123456", "dbtable" -> "file_resource"))
-    jdbcDF.printSchema();
 
+    spark.sparkContext.addJar("C:\\Users\\sdzn-dsj\\.m2\\repository\\mysql\\mysql-connector-java\\5.1.40\\mysql-connector-java-5.1.40.jar");
+
+    val df = spark.sqlContext.load("jdbc", Map("url" -> "jdbc:mysql://192.168.0.118:3306/resource?user=root&password=sdzn123456", "dbtable" -> "file_resource"));
+    df.printSchema();
+    df.select("resource_name").show(10);
+
+    df.createOrReplaceTempView("rs")
+    var sqlDF=spark.sql("select * from rs limit 10");
+    sqlDF.show();
   }
 }
