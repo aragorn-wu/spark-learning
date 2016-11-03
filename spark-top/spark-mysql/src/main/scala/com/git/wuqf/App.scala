0dfa031115838414fd943372b4d31d34c39434ba
deadking01 <wuqingfeng234@163.com>
2016-11-03 17:27:48 +0800

    fix bugs

--- spark-top/spark-mysql/src/main/scala/com/git/wuqf/App.scala
+++ spark-top/spark-mysql/src/main/scala/com/git/wuqf/App.scala
@@ -9,7 +9,7 @@ import org.apache.spark.sql.{SQLContext, SparkSession}
 object App {
 
   def main(args: Array[String]): Unit = {
-    var spark = SparkSession.builder().appName("spark-mysql").master("spark://hadoop01:7077").getOrCreate();
+    val spark = SparkSession.builder().appName("spark-mysql").master("spark://hadoop01:7077").getOrCreate();
     val jdbcDF = spark.sqlContext.load("jdbc", Map("url" -> "jdbc:mysql://192.168.0.118:3306/resource?user=root&password=sdzn123456", "dbtable" -> "file_resource"))
     jdbcDF.printSchema();
 
