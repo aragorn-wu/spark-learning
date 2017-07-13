9c5995d238ce61f66aaf3b72a7a61400fc97dbb7
deadking01 <wuqingfeng234@163.com>
2017-07-13 17:00:10 +0800

    make code easier to read

--- spark-demos/bike-share/src/main/scala/com/git/wuqf/spark/demos/MLRegression.scala
+++ spark-demos/bike-share/src/main/scala/com/git/wuqf/spark/demos/MLRegression.scala
@@ -18,10 +18,10 @@ object MLRegression {
   }
 
 
-
-
   def initLocalSparkConf(): SparkConf = {
-    val conf = new SparkConf().setAppName("spark-bike").setMaster("local[*]").setSparkHome("/opt/bigdata/spark-2.1.1-bin-hadoop2.7")
+    val linuxPath = "/opt/bigdata/spark-2.1.1-bin-hadoop2.7";
+
+    val conf = new SparkConf().setAppName("spark-bike").setMaster("local[*]").setSparkHome(linuxPath)
     return conf;
   }
 
@@ -34,31 +34,30 @@ object MLRegression {
     val records = sc.textFile("src/main/resources/hour.csv").map(_.split(",")).cache()
 
 
+    val mappings = for (i <- Range(2, 10)) yield splitDimension(records, i)
 
-    val mappings = for (i <- Range(2, 10)) yield getMapping(records, i)
+    val limittedVectorLength = sum(mappings.map(_.size))
+    val unLimittedVectorLength = records.first().slice(10, 14).size
+    val totalLength = limittedVectorLength + unLimittedVectorLength
 
-    val cat_len = sum(mappings.map(_.size))
-    val num_len = records.first().slice(10, 14).size
-    val total_len = cat_len + num_len
-
-    //产生训练数据集，按照前文所述处理类别特征和实数特征。
+    //产生训练数据集矩阵，按照前文所述处理类别特征和实数特征。
     val data = records.map { record =>
 
-      val cat_vec = Array.ofDim[Double](cat_len)
+      val limittedVector = Array.ofDim[Double](limittedVectorLength)
       var i = 0
       var step = 0
       for (filed <- record.slice(2, 10)) {
 
         val m = mappings(i)
         val idx = m(filed)
-        cat_vec(idx.toInt + step) = 1.0
+        limittedVector(idx.toInt + step) = 1.0
         i = i + 1
         step = step + m.size
       }
 
-      val num_vec = record.slice(10, 14).map(x => x.toDouble)
+      val unLimittedVector = record.slice(10, 14).map(x => x.toDouble)
 
-      val features = cat_vec ++ num_vec
+      val features = limittedVector ++ unLimittedVector
       val label = record(record.size - 1).toInt
 
 
@@ -67,6 +66,7 @@ object MLRegression {
 
     // val categoricalFeaturesInfo = Map[Int, Int]()
     //val linear_model=DecisionTree.trainRegressor(data,categoricalFeaturesInfo,"variance",5,32)
+
     val linear_model = LinearRegressionWithSGD.train(data, 40, 0.5)
     val true_vs_predicted = data.map(p => (p.label, linear_model.predict(p.features)))
 
@@ -74,8 +74,10 @@ object MLRegression {
     println(true_vs_predicted.take(5).toVector.toString())
   }
 
-  def getMapping(rdd: RDD[Array[String]], idx: Int) :Map[String, Long]= {
-    val md=rdd.map(filed => filed(idx)).distinct().zipWithIndex().collectAsMap()
+  def splitDimension(rdd: RDD[Array[String]], idx: Int): Map[String, Long] = {
+    //rdd.foreach(x=>println(x.length))
+    rdd.map(field => field(idx)).distinct().zipWithIndex().collectAsMap().foreach(println)
+    val md = rdd.map(filed => filed(idx)).distinct().zipWithIndex().collectAsMap()
     return md;
   }
 
