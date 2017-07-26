a8f93f2fc9391b49db35799ab0a106afa9016090
deadking01 <wuqingfeng234@163.com>
2017-07-26 16:32:50 +0800

    movie recommend

--- spark-demos/movie-recommend/src/main/scala/com/git/wuqf/demos/movie/MovieRecommend.scala
+++ spark-demos/movie-recommend/src/main/scala/com/git/wuqf/demos/movie/MovieRecommend.scala
@@ -1,3 +1,18 @@
+package com.git.wuqf.demos.movie
+
+import org.apache.spark.{SparkConf, SparkContext}
+
 object MovieRecommend {
-  
+  def main(args: Array[String]): Unit = {
+    val conf = initLocalSparkConf()
+    val sc = new SparkContext(conf)
+
+  }
+
+  def initLocalSparkConf(): SparkConf = {
+    val linuxPath = "/opt/bigdata/spark-2.1.1-bin-hadoop2.7";
+
+    val conf = new SparkConf().setAppName("movie-recommond").setMaster("local[*]").setSparkHome(linuxPath)
+    return conf;
+  }
 }
--- spark-demos/movie-recommend/src/main/scala/com/git/wuqf/demos/movie/MovieStatistics.scala
+++ spark-demos/movie-recommend/src/main/scala/com/git/wuqf/demos/movie/MovieStatistics.scala
@@ -1,5 +1,33 @@
 package com.git.wuqf.demos.movie
 
-class MovieStatistics {
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.{Row, SparkSession}
 
+object MovieStatistics {
+  def main(args: Array[String]): Unit = {
+    userStatistic(initSparkSession())
+  }
+
+  def userStatistic(sparkSession: SparkSession): Unit = {
+    val userRDD = sparkSession.sparkContext.textFile("spark-demos/movie-recommend/src/main/resources/u.user");
+    val schemaSring = "index age gender job zipcode"
+    val fields = schemaSring.split(" ").map(fieldName => StructField(fieldName, StringType, nullable = true))
+    val schema = StructType(fields)
+    val rowRdd = userRDD.map(_.split("\\|")).map(attributes => Row(attributes(0), attributes(1), attributes(2), attributes(3), attributes(4)))
+    val userDF = sparkSession.createDataFrame(rowRdd, schema)
+    userDF.createOrReplaceTempView("user")
+    val resulults = sparkSession.sql("select age from user")
+    resulults.show()
+  }
+
+  def initSparkSession(): SparkSession = {
+
+    val conf = new SparkConf().setAppName("movie-statistic").setMaster("local[*]");
+
+    val sparkSession = SparkSession.builder()
+      .config(conf)
+      .getOrCreate()
+    return sparkSession;
+  }
 }
