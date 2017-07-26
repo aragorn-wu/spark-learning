4c02cf7f3904bccb013e10c27ef91a5b034d9b48
deadking01 <wuqingfeng234@163.com>
2017-07-26 17:22:29 +0800

    优化代码结构

--- spark-demos/movie-recommend/src/main/scala/com/git/wuqf/demos/movie/SqlConstant.scala
+++ spark-demos/movie-recommend/src/main/scala/com/git/wuqf/demos/movie/SqlConstant.scala
@@ -1,6 +1,10 @@
 package com.git.wuqf.demos.movie
 
 object SqlConstant {
-  val genderStatistic="select distinct gender,count(*) from user group by gender";
-  val ageStatistic="select distinct age,count(*) from user group by age";
+
+  object userSql {
+    val genderStatistic = "select distinct gender,count(*) from user group by gender";
+    val ageStatistic = "select distinct age,count(*) from user group by age";
+  }
+
 }
